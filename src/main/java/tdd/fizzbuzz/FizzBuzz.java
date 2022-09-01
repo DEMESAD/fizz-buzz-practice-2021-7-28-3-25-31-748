ae9a57b7a00115f619dd27eefa7c54903fc433a8
de Mesa, Adrian Kyle <adrian.kyle.de.mesa@oocl.com>
2022-09-01 14:32:23 +0800

    Refactor: Create variables for buzz and FizzBuzz

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,11 +2,17 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     String fizz = "Fizz";
+    String buzz = "Buzz";
+    String fizzBuzz = "FizzBuzz";
     public String countOff(int order) {
-        if (order % 3 == 0) {
+        if (order % 3 == 0 && order % 5 == 0){
+
+            return fizzBuzz;
+        }else if (order % 3 == 0) {
             return fizz;
         } else if (order % 5 == 0) {
-            return "Buzz";
+
+            return buzz;
         }
         return String.valueOf(order);
     }
