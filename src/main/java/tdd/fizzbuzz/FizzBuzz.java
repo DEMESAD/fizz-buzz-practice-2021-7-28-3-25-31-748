253e4b8225a6c9a0afc9b82037bfbfe0a957c4c1
de Mesa, Adrian Kyle <adrian.kyle.de.mesa@oocl.com>
2022-09-01 14:27:48 +0800

    Feat: else-if added for buzz

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,8 +4,9 @@ public class FizzBuzz {
     String fizz = "Fizz";
     public String countOff(int order) {
         if (order % 3 == 0) {
-
             return fizz;
+        } else if (order % 5 == 0) {
+            return "Buzz";
         }
         return String.valueOf(order);
     }
